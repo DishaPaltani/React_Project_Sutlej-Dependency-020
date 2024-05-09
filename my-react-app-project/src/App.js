bb9484febda3f1f4ac966557a32cf8953d849fe5
BabakhalilM <babakhalilmalyam2@gmail.com>
2024-05-10 00:16:55 +0530

    fetching and cards created

---
+++ my-react-app-project/src/App.js
@@ -0,0 +1,12 @@
+import Mycard from './components/fetchdata';
+// import MyComponent from './components/fetchdata';
+
+function App() {
+  return (
+
+      <Mycard> </Mycard>
+      
+  );
+}
+
+export default App;
---
+++ my-react-app-project/src/components/ProductCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ProductCard.module.css';
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="card">
+      <img src={product.image_url} alt={product.title} />
+      <div className="card-body">
+        <h5 className="card-title">{product.title}</h5>
+        <p className="card-text" >{product.description}</p>
+        <p className="card-price">${product.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
