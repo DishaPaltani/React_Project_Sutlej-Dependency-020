2eade0d3669673728cadd48c3e98aca458ee603b
Disha Paltani <paltanikshitij@gmail.com>
2024-05-09 21:09:09 +0530

    day2 commit

---
+++ wine-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+import { Footer } from './components/Footer'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
