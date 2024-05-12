8c4e0b81e28a027cde92e18044ab88fb6272bcec
BabakhalilM <babakhalilmalyam2@gmail.com>
2024-05-12 23:33:03 +0530

    data add to server

--- my-react-app-project/src/M_components/M_fetchdata.jsx
+++ my-react-app-project/src/M_components/M_fetchdata.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-// import ProductCard from './ProductCard';
 
-import ProductCard from './ProductCards';
+import ProductCard from './M_ProductCards';
 
 import "./fetchdata.css";
 
